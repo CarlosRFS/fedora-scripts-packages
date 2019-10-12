69cf695fcb807a476badc57f9462cfe02f34fec7
CarlosRFS <carlosrfs99@gmail.com>
2019-10-11 21:48:42 -0300

    correção no uninstall

--- radeon-profile/uninstall.sh
+++ radeon-profile/uninstall.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+systemctl stop radeon-profile-daemon.service
+systemctl disable radeon-profile-daemon.service
 rm /usr/bin/radeon-profile
 rm /usr/bin/radeon-profile-daemon
 rm /usr/share/pixmaps/radeon-profile.png
